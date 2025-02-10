6349270c55130d47d0db816e46992d46c36217e5
emil <emilebert@icloud.com>
2025-02-10 22:51:05 +0000

    add login route in navbar

--- frontend/layouts/basic-navbar.tsx
+++ frontend/layouts/basic-navbar.tsx
@@ -4,150 +4,154 @@ import type {NavbarProps} from "@heroui/react";
 
 import React from "react";
 import {
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  NavbarMenu,
-  NavbarMenuItem,
-  NavbarMenuToggle,
-  Link,
-  Button,
-  Divider,
-  cn,
+    Navbar,
+    NavbarBrand,
+    NavbarContent,
+    NavbarItem,
+    NavbarMenu,
+    NavbarMenuItem,
+    NavbarMenuToggle,
+    Link,
+    Button,
+    Divider,
+    cn,
 } from "@heroui/react";
 import {Icon} from "@iconify/react";
 
-import { CoreLogoWhite} from "../components/social";
-import { ThemeSwitch } from "@/components/theme-switch";
-
-const menuItems = [
-  "Home",
-  "Wiki",
-  "About us",
-];
+import {CoreLogoWhite} from "../components/social";
+import {ThemeSwitch} from "@/components/theme-switch";
 
 const BasicNavbar = React.forwardRef<HTMLElement, NavbarProps>(
-  ({classNames = {}, ...props}, ref) => {
-    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-    const pathname = typeof window !== 'undefined' ? window.location.pathname : '/';
+    ({classNames = {}, ...props}, ref) => {
+        const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+        const pathname = typeof window !== 'undefined' ? window.location.pathname : '/';
 
-    return (
-      <Navbar
-        ref={ref}
-        {...props}
-        classNames={{
-          base: cn("border-default-100 bg-transparent", {
-            "bg-default-200/50 dark:bg-default-100/50": isMenuOpen,
-          }),
-          wrapper: "w-full justify-center",
-          item: "hidden md:flex",
-          ...classNames,
-        }}
-        height="60px"
-        isMenuOpen={isMenuOpen}
-        onMenuOpenChange={setIsMenuOpen}
-      >
-        {/* Left Content */}
-        <NavbarBrand>
-          <Link href="/">
-            <img src="/logo-white.svg" alt="CORE" className="w-10 h-10" />
-            <span className="ml-2 text-small font-medium text-default-foreground">CORE</span>
-          </Link>
-        </NavbarBrand>
-
-        {/* Center Content */}
-        <NavbarContent justify="center">
-          <NavbarItem>
-            <Link 
-              className={cn("text-default-500", {
-                "font-bold text-default-foreground": pathname === "/"
-              })} 
-              href="/" 
-              size="sm"
+        return (
+            <Navbar
+                ref={ref}
+                {...props}
+                classNames={{
+                    base: cn("border-default-100 bg-transparent", {
+                        "bg-default-200/50 dark:bg-default-100/50": isMenuOpen,
+                    }),
+                    wrapper: "w-full justify-center",
+                    item: "hidden md:flex",
+                    ...classNames,
+                }}
+                height="60px"
+                isMenuOpen={isMenuOpen}
+                onMenuOpenChange={setIsMenuOpen}
             >
-              Home
-            </Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link 
-              className={cn("text-default-500", {
-                "font-bold text-default-foreground": pathname === "/wiki/season1"
-              })} 
-              href="https://wiki.coregame.de" 
-              size="sm"
-            >
-              Wiki
-            </Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link 
-              className={cn("text-default-500", {
-                "font-bold text-default-foreground": pathname === "/about"
-              })} 
-              href="/about" 
-              size="sm"
-            >
-              About Us
-            </Link>
-          </NavbarItem>
-        </NavbarContent>
+                {/* Left Content */}
+                <NavbarBrand>
+                    <Link href="/">
+                        <img src="/logo-white.svg" alt="CORE" className="w-10 h-10"/>
+                        <span className="ml-2 text-small font-medium text-default-foreground">CORE</span>
+                    </Link>
+                </NavbarBrand>
 
-        {/* Right Content */}
-        <NavbarContent className="hidden md:flex" justify="end">
-          <NavbarItem className="ml-2 !flex gap-2">
-            <ThemeSwitch />
-            <Button
-              className="bg-default-foreground font-medium text-background"
-              color="secondary"
-              endContent={<Icon icon="solar:alt-arrow-right-linear" />}
-              radius="full"
-              variant="flat"
-              onClick={() => window.open("https://wiki.coregame.de", "_blank")}
-            >
-              Get Started
-            </Button>
-          </NavbarItem>
-        </NavbarContent>
+                {/* Center Content */}
+                <NavbarContent justify="center">
+                    <NavbarItem>
+                        <Link
+                            className={cn("text-default-500", {
+                                "font-bold text-default-foreground": pathname === "/"
+                            })}
+                            href="/"
+                            size="sm"
+                        >
+                            Home
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem>
+                        <Link
+                            className={cn("text-default-500", {
+                                "font-bold text-default-foreground": pathname === "/wiki/season1"
+                            })}
+                            href="https://wiki.coregame.de"
+                            size="sm"
+                        >
+                            Wiki
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem>
+                        <Link
+                            className={cn("text-default-500", {
+                                "font-bold text-default-foreground": pathname === "/about"
+                            })}
+                            href="/about"
+                            size="sm"
+                        >
+                            About Us
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem>
+                        <Link
+                            className={cn("text-default-500", {
+                                "font-bold text-default-foreground": pathname === "/login"
+                            })}
+                            href="/login"
+                        >
+                            Login
+                        </Link>
+                    </NavbarItem>
+                </NavbarContent>
 
-        <NavbarMenuToggle className="text-default-400 md:hidden" />
+                {/* Right Content */}
+                <NavbarContent className="hidden md:flex" justify="end">
+                    <NavbarItem className="ml-2 !flex gap-2">
+                        <ThemeSwitch/>
+                        <Button
+                            className="bg-default-foreground font-medium text-background"
+                            color="secondary"
+                            endContent={<Icon icon="solar:alt-arrow-right-linear"/>}
+                            radius="full"
+                            variant="flat"
+                            onClick={() => window.open("https://wiki.coregame.de", "_blank")}
+                        >
+                            Get Started
+                        </Button>
+                    </NavbarItem>
+                </NavbarContent>
 
-        <NavbarMenu
-          className="top-[calc(var(--navbar-height)_-_1px)] max-h-fit bg-default-200/50 pb-6 pt-6 shadow-medium backdrop-blur-md backdrop-saturate-150 dark:bg-default-100/50"
-          motionProps={{
-            initial: {opacity: 0, y: -20},
-            animate: {opacity: 1, y: 0},
-            exit: {opacity: 0, y: -20},
-            transition: {
-              ease: "easeInOut",
-              duration: 0.2,
-            },
-          }}
-        >
-          <NavbarMenuItem className="mb-4">
-            <Button fullWidth as={Link} className="bg-foreground text-background" href="/#">
-              Get Started
-            </Button>
-          </NavbarMenuItem>
-          <NavbarMenuItem>
-            <Link className="mb-2 w-full text-default-500" href="/" size="md">
-              Home
-            </Link>
-          </NavbarMenuItem>
-          <NavbarMenuItem>
-            <Link className="mb-2 w-full text-default-500" href="https://wiki.coregame.de" size="md">
-              Wiki
-            </Link>
-          </NavbarMenuItem>
-          <NavbarMenuItem>
-            <Link className="mb-2 w-full text-default-500" href="/about" size="md">
-              About us
-            </Link>
-          </NavbarMenuItem>
-        </NavbarMenu>
-      </Navbar>
-    );
-  },
+                <NavbarMenuToggle className="text-default-400 md:hidden"/>
+
+                <NavbarMenu
+                    className="top-[calc(var(--navbar-height)_-_1px)] max-h-fit bg-default-200/50 pb-6 pt-6 shadow-medium backdrop-blur-md backdrop-saturate-150 dark:bg-default-100/50"
+                    motionProps={{
+                        initial: {opacity: 0, y: -20},
+                        animate: {opacity: 1, y: 0},
+                        exit: {opacity: 0, y: -20},
+                        transition: {
+                            ease: "easeInOut",
+                            duration: 0.2,
+                        },
+                    }}
+                >
+                    <NavbarMenuItem className="mb-4">
+                        <Button fullWidth as={Link} className="bg-foreground text-background" href="/#">
+                            Get Started
+                        </Button>
+                    </NavbarMenuItem>
+                    <NavbarMenuItem>
+                        <Link className="mb-2 w-full text-default-500" href="/" size="md">
+                            Home
+                        </Link>
+                    </NavbarMenuItem>
+                    <NavbarMenuItem>
+                        <Link className="mb-2 w-full text-default-500" href="https://wiki.coregame.de" size="md">
+                            Wiki
+                        </Link>
+                    </NavbarMenuItem>
+                    <NavbarMenuItem>
+                        <Link className="mb-2 w-full text-default-500" href="/about" size="md">
+                            About us
+                        </Link>
+                    </NavbarMenuItem>
+                </NavbarMenu>
+            </Navbar>
+        );
+    },
 );
 
 BasicNavbar.displayName = "BasicNavbar";

7e5a89074eae80f019bfba21068f319af6acdbf7
Jonas Götz <jonas@42heilbronn.de>
2025-02-13 17:30:01 +0000

    remove test code

--- k8s-service/cmd/server/main.go
+++ k8s-service/cmd/server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"log"
 	"net"
 
@@ -17,28 +16,13 @@ func main() {
 		log.Fatalf("failed to create k8s service: %v", err)
 	}
 
-	// k8sService.CreateDeployment(context.Background(), "game-server", "nginx:latest", map[string]string{"app": "game-server"})
-	pods, err := k8sService.GetPodsByLabel(context.Background(), "app=game-server")
-	if err != nil {
-		log.Fatalf("failed to get pods: %v", err)
-	}
-
-	log.Printf("pods: %v", pods)
-
-	logs, err := k8sService.StreamPodLogs(context.Background(), pods.Items[0].Name)
-	if err != nil {
-		log.Fatalf("failed to stream logs: %v", err)
-	}
-
-	log.Printf("logs: %v", logs)
-
 	lis, err := net.Listen("tcp", ":9000")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
 	s := grpc.NewServer()
-	pb.RegisterGameServiceServer(s, service.NewGameService())
+	pb.RegisterGameServiceServer(s, service.NewGameService(k8sService))
 
 	log.Printf("Server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {

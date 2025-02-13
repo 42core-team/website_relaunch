78557f7509fd9d85786292bd06724e39dc7e3a58
Jonas Götz <jonas@42heilbronn.de>
2025-02-13 17:09:13 +0000

    test k8s service stuff

--- k8s-service/cmd/server/main.go
+++ k8s-service/cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 
@@ -10,6 +11,27 @@ import (
 )
 
 func main() {
+
+	k8sService, err := service.NewK8sService("/root/.kube/config")
+	if err != nil {
+		log.Fatalf("failed to create k8s service: %v", err)
+	}
+
+	// k8sService.CreateDeployment(context.Background(), "game-server", "nginx:latest", map[string]string{"app": "game-server"})
+	pods, err := k8sService.GetPodsByLabel(context.Background(), "app=game-server")
+	if err != nil {
+		log.Fatalf("failed to get pods: %v", err)
+	}
+
+	log.Printf("pods: %v", pods)
+
+	logs, err := k8sService.StreamPodLogs(context.Background(), pods.Items[0].Name)
+	if err != nil {
+		log.Fatalf("failed to stream logs: %v", err)
+	}
+
+	log.Printf("logs: %v", logs)
+
 	lis, err := net.Listen("tcp", ":9000")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)

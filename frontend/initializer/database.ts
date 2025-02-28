009e28ad2a6e2e393e92c2fa27cb7b9a53fe1719
Paul Großmann <git@paulgrossmann.de>
2025-02-28 17:48:33 +0000

    Update database configuration and gitignore

--- frontend/initializer/database.ts
+++ frontend/initializer/database.ts
@@ -9,7 +9,8 @@ import {NotificationEntity} from "@/entities/notifications.entity";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    port: 5432,
+    host: process.env.DB_HOST || "localhost",
+    port: parseInt(process.env.DB_PORT || "5432"),
     username: process.env.DB_USER || "postgres",
     password: process.env.DB_PASSWORD || "postgres",
     database: process.env.DB_NAME || "postgres",

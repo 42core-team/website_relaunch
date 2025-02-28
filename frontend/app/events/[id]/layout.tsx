627f6bc61d76e3c8ea0259f5f730315cb93e8a31
Paul Großmann <git@paulgrossmann.de>
2025-02-28 22:15:31 +0000

    Remove unnecessary imports and adjust layout padding

--- frontend/app/events/[id]/layout.tsx
+++ frontend/app/events/[id]/layout.tsx
@@ -1,7 +1,4 @@
 import React from "react";
-import {HeroUIProvider} from "@heroui/system";
-import {ThemeProvider as NextThemesProvider} from "next-themes";
-import DefaultLayout from "@/layouts/default";
 import EventLayout from "@/layouts/event";
 
 export default function App({children}: {

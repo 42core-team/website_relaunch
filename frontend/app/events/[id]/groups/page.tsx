f39977846c478937d55c4373ac66bb80b864c7c1
Emil Ebert <emilebert@icloud.com>
2025-03-14 15:19:10 +0100

    add clientHeroui component wrapper for server compatibility

--- frontend/app/events/[id]/groups/page.tsx
+++ frontend/app/events/[id]/groups/page.tsx
@@ -1,6 +1,10 @@
+import { Button } from "@/components/clientHeroui";
+
+
 export default async function page(){
     return (
         <div>
+            <Button color={"primary"}>next </Button>
             <h1>Group phase</h1>
             <p>Group phase is the first phase of the tournament where teams are divided into groups and play against each other.</p>
         </div>

b858dc9b4ebd04e1854833f5dd81b1219b18dfe7
Paul Großmann <git@paulgrossmann.de>
2025-05-06 22:56:44 +0000

    refactor next.config.js to remove image settings and add logging configuration

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,18 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "nextuipro.nyc3.cdn.digitaloceanspaces.com",
-      },
-    ],
-  },
-  eslint: {
-    // NOTE: This allows production builds to successfully complete
-    // even if your project has ESLint errors.
-    // ignoreDuringBuilds: true,
+  logging: {
+    level: "info",
+    format: "json",
   },
 };
 

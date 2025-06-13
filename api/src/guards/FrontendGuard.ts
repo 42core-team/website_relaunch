b21bda4d9efaec2ad18ed04d053ae5fab0b9c028
Emil Ebert <emilebert@icloud.com>
2025-06-13 23:22:19 +0200

    feat: add UserId decorator to extract user ID from request headers

--- api/src/guards/FrontendGuard.ts
+++ api/src/guards/FrontendGuard.ts
@@ -1,6 +1,14 @@
-import {CanActivate, ExecutionContext, UnauthorizedException} from "@nestjs/common";
+import {CanActivate, createParamDecorator, ExecutionContext, UnauthorizedException} from "@nestjs/common";
 import {Observable} from "rxjs";
 import {ConfigService} from "@nestjs/config";
+
+
+export const UserId = createParamDecorator(
+    (data: unknown, ctx: ExecutionContext) => {
+        const request = ctx.switchToHttp().getRequest();
+        return request.headers['userid'] || request.userId;
+    },
+);
 
 export class FrontendGuard implements CanActivate {
     constructor(private readonly config: ConfigService) {
@@ -9,9 +17,8 @@ export class FrontendGuard implements CanActivate {
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
         const request = context.switchToHttp().getRequest();
         const authorization = request.headers.authorization;
-        if (!authorization && authorization !== this.config.get("FRONTEND_SECRET")) {
+        if (!authorization && authorization !== this.config.get("FRONTEND_SECRET"))
             throw new UnauthorizedException()
-        }
         return true;
     }
 }

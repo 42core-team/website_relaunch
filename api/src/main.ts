cee80ab1316f8d7e5f86e7d47520f9fc3bbc8837
Emil Ebert <emilebert@icloud.com>
2025-06-25 23:48:52 +0200

    show swagger only in dev mode

--- api/src/main.ts
+++ api/src/main.ts
@@ -11,14 +11,17 @@ async function bootstrap() {
     app.useGlobalFilters(new TypeormExceptionFilter());
     app.enableCors();
 
-    const config = new DocumentBuilder()
-        .setTitle('CORE game API')
-        .setDescription('This is the API for the CORE game backend.')
-        .setVersion('1.0')
-        .addTag('core')
-        .build();
-    const documentFactory = () => SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, documentFactory);
+    if (process.env.NODE_ENV === 'development') {
+        const config = new DocumentBuilder()
+            .setTitle('CORE game API')
+            .setDescription('This is the API for the CORE game backend.')
+            .setVersion('1.0')
+            .addTag('core')
+            .build();
+        const documentFactory = () => SwaggerModule.createDocument(app, config);
+        SwaggerModule.setup('api', app, documentFactory);
+    }
+
 
     await app.listen(process.env.PORT ?? 4000);
 }

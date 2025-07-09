32a73ea383f03edbd6c8071f58665c099ff36e12
Paul Großmann <git@paulgrossmann.de>
2025-07-09 21:09:50 +0000

    fix: update RabbitMQ connection string to use host.docker.internal

--- k8s-service-gen/internal/config/config.go
+++ k8s-service-gen/internal/config/config.go
@@ -2,16 +2,17 @@ package config
 
 import (
 	"context"
+	"log"
+
 	"github.com/joho/godotenv"
 	"github.com/sethvargo/go-envconfig"
-	"log"
 )
 
 type Config struct {
 	Addr        string  `env:"ADDR, default=:9000"`
 	KubePath    *string `env:"KUBE_PATH"`
 	Namespace   string  `env:"NAMESPACE, default=coregame"`
-	RabbitMQ    string  `env:"RABBITMQ, default=amqp://guest:guest@rabbitmq:5672/"`
+	RabbitMQ    string  `env:"RABBITMQ, default=amqp://guest:guest@host.docker.internal:5672/"`
 	ServerImage string  `env:"SERVER_IMAGE, default=ghcr.io/42core-team/game-server:dev"`
 	BotImage    string  `env:"BOT_IMAGE, default=ghcr.io/42core-team/my-core-bot:dev"`
 }

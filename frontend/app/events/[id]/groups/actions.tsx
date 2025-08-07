ab029062de7b4f9a60fe7034ceda91d172f237bb
Emil Ebert <emilebert@icloud.com>
2025-08-07 23:44:04 +0200

    refactor: remove unused tournament match buttons and parameters

--- frontend/app/events/[id]/groups/actions.tsx
+++ frontend/app/events/[id]/groups/actions.tsx
@@ -1,20 +1,7 @@
 "use client";
 
-import { Button } from "@/components/clientHeroui";
 import { useParams } from "next/navigation";
-import { startSwissMatches } from "@/app/actions/tournament";
 
 export default function Actions() {
-  const params = useParams();
-
-  return (
-    <>
-      <Button
-        onPress={() => startSwissMatches(params.id as string)}
-        color={"primary"}
-      >
-        Progress Tournament
-      </Button>
-    </>
-  );
+  return <></>;
 }

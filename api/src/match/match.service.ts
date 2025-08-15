7d7438abe478456920d3b57fd3d29971039e0e75
Emil Ebert <emilebert@icloud.com>
2025-08-15 13:52:42 +0200

    change game server version

--- api/src/match/match.service.ts
+++ api/src/match/match.service.ts
@@ -258,7 +258,7 @@ export class MatchService {
 
         this.gameQueue.emit("new_match", {
             id: matchId,
-            image: "ghcr.io/42core-team/game-server:dev-e1eebc586931037b8e193f3a134dfa7d90b36eed",
+            image: "ghcr.io/42core-team/game-server:dev-e4ef663bdb652610f671669b7de4e64ac8216e4e",
             bots: [
                 {
                     id: match.teams[0].id,

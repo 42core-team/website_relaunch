b37c99a512562e9c81bf134f1f4bc65ab64160e6
Emil Ebert <emilebert@icloud.com>
2025-08-16 12:24:04 +0200

    fix #109

--- api/src/match/match.service.ts
+++ api/src/match/match.service.ts
@@ -254,8 +254,6 @@ export class MatchService {
         const orgSecret = this.githubApiService.decryptSecret(event.githubOrgSecret);
         const repoPrefix = `https://${orgName}:${orgSecret}@github.com/${orgName}/`;
 
-        this.logger.log("prefix: " + repoPrefix);
-
         this.gameQueue.emit("new_match", {
             id: matchId,
             image: "ghcr.io/42core-team/game-server:dev-e4ef663bdb652610f671669b7de4e64ac8216e4e",

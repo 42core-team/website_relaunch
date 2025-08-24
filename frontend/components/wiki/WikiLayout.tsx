1e21271a3486ffde485aefff5cc069fd65bf17f9
Paul Großmann <git@paulgrossmann.de>
2025-08-24 15:42:17 +0200

    chore: ran pnpm update

--- frontend/components/wiki/WikiLayout.tsx
+++ frontend/components/wiki/WikiLayout.tsx
@@ -57,7 +57,7 @@ export function WikiLayout({
       <div
         className={`
         fixed lg:sticky top-[60px] left-0 z-50 lg:z-0
-        w-64 h-[calc(100vh-60px)] 
+        w-64 h-[calc(100vh-60px)]
         transform lg:transform-none transition-transform duration-300 ease-in-out
         ${isMobileNavOpen ? "translate-x-0" : "-translate-x-full lg:translate-x-0"}
         flex-shrink-0

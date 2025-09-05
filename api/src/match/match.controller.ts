8ab39ecd3b242ca1603bf3ebbef9e1bd71cbecb3
Paul Großmann <git@paulgrossmann.de>
2025-09-06 00:56:15 +0200

    fix: api event controller using the frontendguard

--- api/src/match/match.controller.ts
+++ api/src/match/match.controller.ts
@@ -10,7 +10,6 @@ import {
     UseGuards
 } from '@nestjs/common';
 import {MatchService} from "./match.service";
-import {Ctx, EventPattern, Payload, RmqContext} from "@nestjs/microservices";
 import {EventService} from "../event/event.service";
 import {EventState} from "../event/entities/event.entity";
 import {FrontendGuard, UserId} from "../guards/FrontendGuard";
@@ -27,46 +26,6 @@ export class MatchController {
     }
 
     private logger = new Logger("MatchController");
-
-    @EventPattern("game_server")
-    async getNotifications(@Payload() data: {
-        team_results: {
-            id: string,
-            name: string,
-            place: number
-        }[]
-        game_id: string,
-        stats: {
-            actions_executed: number,
-            damage_deposits: number,
-            gempiles_destroyed: number,
-            damage_total: number,
-            gems_gained: number,
-            damage_walls: number,
-            damage_cores: number,
-            units_spawned: number,
-            tiles_traveled: number
-            damage_self: number
-            damage_units: number
-            walls_destroyed: number
-            gems_transferred: number
-            units_destroyed: number
-            cores_destroyed: number
-            damage_opponent: number
-        }
-        BOT_ID_MAPPING: {
-            [key: string]: string
-        }
-    }, @Ctx() context: RmqContext) {
-        try {
-            this.logger.log(`Processing match result for game_id: ${data.game_id} with results: ${JSON.stringify(data.team_results, null, 2)}`);
-            this.logger.log("data: " + JSON.stringify(data, null, 2));
-            const winnerId = data.BOT_ID_MAPPING[data.team_results.sort((a, b) => a.place - b.place)[0].id];
-            await this.matchService.processMatchResult(data.game_id, winnerId, data.stats);
-        } catch (e) {
-            this.logger.error("Error processing match result", e);
-        }
-    }
 
     @Get("swiss/:eventId")
     getSwissMatches(

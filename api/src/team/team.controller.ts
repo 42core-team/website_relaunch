42a319d4379b4be3a6f033feccd80d7f73d4dd81
Paul Großmann <git@paulgrossmann.de>
2025-09-06 21:45:41 +0200

    fix: that you can find a user that is in a team in another event

--- api/src/team/team.controller.ts
+++ api/src/team/team.controller.ts
@@ -19,7 +19,6 @@ import {InviteUserDto} from "./dtos/inviteUserDto";
 import {UserService} from "../user/user.service";
 import {EventService} from "../event/event.service";
 import {UserGuard} from "../guards/UserGuard";
-import {EventPattern} from "@nestjs/microservices";
 
 // TODO: create pipe or guard for user team validation, so we don't have to check for team existence in every endpoint
 @UseGuards(FrontendGuard)

f27cbee4ef72bac9f9cad1ae95c006e44013880d
Paul Großmann <git@paulgrossmann.de>
2025-09-09 00:04:27 +0200

    chore: add dotenv back dependency and configure dotenv in typeOrm.config.ts

--- api/typeOrm.config.ts
+++ api/typeOrm.config.ts
@@ -1,6 +1,10 @@
 import { ConfigService } from "@nestjs/config";
 import {DatabaseConfig} from "./src/DatabaseConfig";
 
+import { config } from "dotenv";
+
+config();
+
 const configService = new ConfigService();
 const databaseConfig = new DatabaseConfig(configService);
 

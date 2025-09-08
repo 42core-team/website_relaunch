19c84fe51add5658601e815e7c52fa6e8c0ea564
Emil Ebert <emilebert@icloud.com>
2025-09-08 19:37:13 +0200

    remove unused import

--- api/src/github-api/github-api.module.ts
+++ api/src/github-api/github-api.module.ts
@@ -1,23 +1,7 @@
 import { Module } from "@nestjs/common";
-import { ClientsModule, Transport } from "@nestjs/microservices";
 import { GithubApiService } from "./github-api.service";
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: 'GITHUB_SERVICE',
-        transport: Transport.RMQ,
-        options: {
-          urls: [process.env.RABBITMQ_URL || 'amqp://localhost:5672'],
-          queue: 'github_queue',
-          queueOptions: {
-            durable: true,
-          },
-        },
-      },
-    ]),
-  ],
   providers: [GithubApiService],
   exports: [GithubApiService],
 })

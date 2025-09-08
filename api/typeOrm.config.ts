ccc32624e40f02c7dafa3d2ddc7e034d74cc9bc7
Paul Großmann <git@paulgrossmann.de>
2025-09-08 23:40:15 +0200

    fix: remove dotenv and update deps

--- api/typeOrm.config.ts
+++ api/typeOrm.config.ts
@@ -1,8 +1,5 @@
-import { config } from "dotenv";
 import { ConfigService } from "@nestjs/config";
 import {DatabaseConfig} from "./src/DatabaseConfig";
-
-config();
 
 const configService = new ConfigService();
 const databaseConfig = new DatabaseConfig(configService);

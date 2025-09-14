34de091af7fbb14c8fd6522992ff85b1b14e6afd
Emil Ebert <emilebert@icloud.com>
2025-09-14 20:38:59 +0200

    Revert "feat: prevent joining events that have not yet started for unauthenticated users"
    This reverts commit 04044567339e962d2d643d6324f0ffcfbcb7a5b6.

--- frontend/app/events/EventsTabs.tsx
+++ frontend/app/events/EventsTabs.tsx
@@ -1,36 +1,29 @@
 "use client";
 
-import {Tab, Tabs} from "@heroui/react";
+import { Tab, Tabs } from "@heroui/react";
 import EventsTable from "@/app/events/EventTable";
-import {Event} from "@/app/actions/event";
+import { Event } from "@/app/actions/event";
 
 export default function EventsTabs({
-                                       myEvents,
-                                       allEvents,
-                                       isLoggedIn
-                                   }: {
-    myEvents: Event[];
-    allEvents: Event[];
-    isLoggedIn: boolean;
+  myEvents,
+  allEvents,
+}: {
+  myEvents: Event[];
+  allEvents: Event[];
 }) {
-    if (!isLoggedIn) {
-        return (
-            <EventsTable events={allEvents}/>
-        )
-    }
-    return (
-        <Tabs
-            aria-label="Events tabs"
-            className="w-full"
-            variant="underlined"
-            defaultSelectedKey={myEvents.length ? "my" : "all"}
-        >
-            <Tab key="my" title={`My Events (${myEvents.length})`}>
-                <EventsTable events={myEvents}/>
-            </Tab>
-            <Tab key="all" title={`All Events (${allEvents.length})`}>
-                <EventsTable events={allEvents}/>
-            </Tab>
-        </Tabs>
-    );
+  return (
+    <Tabs
+      aria-label="Events tabs"
+      className="w-full"
+      variant="underlined"
+      defaultSelectedKey={myEvents.length ? "my" : "all"}
+    >
+      <Tab key="my" title={`My Events (${myEvents.length})`}>
+        <EventsTable events={myEvents} />
+      </Tab>
+      <Tab key="all" title={`All Events (${allEvents.length})`}>
+        <EventsTable events={allEvents} />
+      </Tab>
+    </Tabs>
+  );
 }
